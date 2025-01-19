4decfea4809a4c5195ddcffb1c4b2307a04bb9ff
jimfoxes <jimfoxes21@yandex.ru>
2025-01-20 03:10:02 +0500

    Добавлен функционал ответа на комментарий и проверки на ввод HTML кода

---
+++ modules/renderComments.js
@@ -0,0 +1,30 @@
+import { comments } from "./comments.js";
+import { initLikeListeners, initReplyListeners } from "./initCommentListeners.js";
+export const renderComments = () => {
+    const commentsList = document.querySelector('.comments')
+
+    commentsList.innerHTML = comments
+        .map((comment, index) => {
+            return `<li class="comment" data-index="${index}"> 
+                <div class="comment-header"> 
+                  <div>${comment.name}</div> 
+                  <div>${comment.date}</div> 
+                </div> 
+                <div class="comment-body"> 
+                  <div class="comment-text">${comment.text}</div> 
+                </div> 
+                <div class="comment-footer"> 
+                  <div class="likes"> 
+                    <span class="likes-counter">${comment.likes}</span> 
+                    <button class="like-button ${comment.isLiked ? '-active-like' : ''}" data-index="${index}"></button> 
+                  </div> 
+                </div>
+              </li> `
+        })
+        .join('')
+
+        initLikeListeners(renderComments)
+        initReplyListeners()
+
+        
+}
